13be3bbca17d1c29ced435cb483ce17e83490656
BrovYV <101995442+BrovYV@users.noreply.github.com>
2024-04-25 15:31:02 +0300

    Deleted

--- obj/Debug/net8.0/FinalTaskBlock1.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinalTaskBlock1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalTaskBlock1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c074cba4b2c5694227cff23ffe16111ff81b5571")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d27c5290ac9bee88b292aa869e0386dc56a1df")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalTaskBlock1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalTaskBlock1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
